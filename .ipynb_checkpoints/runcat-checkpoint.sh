6c29d87aee345eafba38af372735eaa03db3f672
Marilyn Waldman <mdwaldman22@gmail.com>
2021-05-28 17:24:20 -0600

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/runcat-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-# RUN to set up environment with all the packages
-
-#conda env create -f environment.yml # This command will create an environment
-conda env create --name ml --file=environment.yml
-#conda activate ml
-source activate ml
-  
-python mycategorical.py
